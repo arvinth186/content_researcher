5ee3aa5e5087d217cf2cb5a24dd9205c449ba92c
Arvinth <59131788+arvinth186@users.noreply.github.com>
2026-06-13 00:15:30 +0530

    Upadted CORS

--- src/content_researcher/api/main.py
+++ src/content_researcher/api/main.py
@@ -2,11 +2,20 @@ from fastapi import FastAPI
 from dotenv import load_dotenv
 from pydantic import BaseModel
 from src.content_researcher.api.runner import generate_article
+from fastapi.middleware.cors import CORSMiddleware
 
 app= FastAPI(
     title="Content Researcher API",
     description="API for multi-agent content research and writing using CrewAI",
     version="1.0.0"
+)
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
 )
 
 class ResearchRequest(BaseModel):
